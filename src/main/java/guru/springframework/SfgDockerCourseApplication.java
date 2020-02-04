7fc52dd4b9af84b932de61e395ec439221b1c8d5
guidobarra <guidobarra18@gmail.com>
2020-02-04 10:19:07 -0300

    cambio de URL para el actuator 2.0

--- src/main/java/guru/springframework/SfgDockerCourseApplication.java
+++ src/main/java/guru/springframework/SfgDockerCourseApplication.java
@@ -3,6 +3,7 @@ package guru.springframework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//tengo que hacer un commit
 @SpringBootApplication
 public class SfgDockerCourseApplication {
 
